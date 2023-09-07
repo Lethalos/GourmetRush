0ce6905fcd44af5be02b94a8dff9204eb044c1bc
Kaan Ozdogan <necdetkaanoz@hotmail.com>
2023-09-07 09:15:57 +0300

    Add put items on counters

--- Assets/Game/Scripts/ClearCounter.cs
+++ Assets/Game/Scripts/ClearCounter.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] KitchenObjectSO kitchenObjectSO;
+    [SerializeField] Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
+
     public void Interact()
     {
-        Debug.Log("sa");
+        if(kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.localPosition = Vector3.zero;
+
+            Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+        }
     }
 }
